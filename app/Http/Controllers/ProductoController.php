0284d2aed587aa31b2059f5518cb44cde71650a7
elvisentuchabe <vromerosaiz@gmail.com>
2026-02-07 15:45:12 +0100

    Demasiados fallos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -30,8 +30,34 @@ class ProductoController extends Controller
     
     public function store(Request $request)
     {
-        $producto = Producto::create($request->all());
+        // Validación estricta
+        $datos = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'precio' => 'required|numeric|min:0',
+            'categoria' => 'nullable|string',
+            'imagen' => 'nullable|string',
+            'descripcion' => 'nullable|string',
+            'stock' => 'integer|min:0'
+        ]);
+
+        $producto = Producto::create($datos);
         return response()->json($producto, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::find($id);
+        if (!$producto) return response()->json(['mensaje' => 'No encontrado'], 404);
+
+        $datos = $request->validate([
+            'nombre' => 'sometimes|required|string|max:255',
+            'precio' => 'sometimes|required|numeric|min:0',
+            'categoria' => 'nullable|string',
+            'stock' => 'integer|min:0'
+        ]);
+
+        $producto->update($datos);
+        return response()->json($producto, 200);
     }
 
     
